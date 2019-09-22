ae62ca5676d651d5f486a9ca1f4499b210216b92
Koekin Yaroslav <ykoekin@gmail.com>
2019-09-22 23:03:08 +0300

    Добавил таблицу кодов.

---
+++ Coding/Coding/CodeTable.cs
@@ -0,0 +1,13 @@
+﻿namespace Coding
+{
+    /// <summary>
+    /// Представляет собой таблицу кодов с лексемами, знаками операций и другими символами.
+    /// </summary>
+    class CodeTable
+    {
+        public char[] commandChars { get; set; } = { };
+        public string[] Terminals { get; set; } = { };
+        public string[] Nonterminals { get; set; } = { };
+        public string[] Semantics { get; set; } = { };
+    }
+}
--- Coding/Coding/ConvolutionViewModel.cs
+++ Coding/Coding/ConvolutionViewModel.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Coding
+﻿namespace Coding
 {
-
     class ConvolutionViewModel
     {
         /// <summary>
         /// Исходное представление грамматики.
         /// </summary>
-        public string GrammaticalText { get; set; } = "";
-        public string NumericalConvolution { get; set; } = "";    
+        public string GrammaticalText { get; set; } = string.Empty;
+        public string NumericalConvolution { get; set; } = string.Empty;
     }
 }
