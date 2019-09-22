654737662d1bfa3d05200114dd092175ca9b1ea4
Koekin Yaroslav <ykoekin@gmail.com>
2019-09-22 16:22:45 +0300

    Создал WPF проект.

---
+++ Coding/Coding/ConvolutionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding
+{
+
+    class ConvolutionViewModel
+    {
+        /// <summary>
+        /// Исходное представление грамматики.
+        /// </summary>
+        public string GrammaticalText { get; set; } = "";
+        public string NumericalConvolution { get; set; } = "";
+
+
+    }
+}
