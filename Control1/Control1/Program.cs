e816155740f59fc6e9780e679daf05aec0063309
Koekin Yaroslav <ykoekin@gmail.com>
2019-10-01 22:31:15 +0300

    Реализовал алфавитный указатель.

---
+++ Control1/Control1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Control1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file path to read text");
+            string fileReaderPath = Console.ReadLine();
+            if (!File.Exists(fileReaderPath))
+            {
+                Console.WriteLine(String.Format("File does not exist: {0}", fileReaderPath));
+
+                return;
+            }
+
+            Console.WriteLine("Enter the file path to write dictionary");
+            string fileWriterPath = Console.ReadLine();
+
+            Concordance concordance = new Concordance();
+
+            using (StreamReader fileReader = new StreamReader(fileReaderPath))
+            {
+                concordance.ReadText(fileReader);
+            }
+
+            using (StreamWriter fileWriter = new StreamWriter(fileWriterPath))
+            {
+                concordance.WriteText(fileWriter);
+            }
+
+        }
+    }
+}
